930744d9b24a2fd87b15cef0412b4776c28b4831
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-01-05 03:16:52 -0800

    add data.R for docs

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' Saudi Regions, Cities and Districts
+#'
+#' A dataset containing all Saudi regions with its cities and districts.
+#'
+#' @format A data frame with 2087 rows and 7 variables: each row is a district.
+#' \describe{
+#'   \item{id}{a unique id}
+#'   \item{region.name}{The name of the region, 13 regions}
+#'   \item{city.name}{City name}
+#'   \item{district.name}{The District Name}
+#'   \item{direction.name}{}
+#'   \item{direction.name}{}
+#'   \item{city.id}{}
+#'   \item{direction.id}{}
+#' }
+#' @source \url{https://github.com/usefksa/Saudi_GIS_Data/}
+"saudiregions"
